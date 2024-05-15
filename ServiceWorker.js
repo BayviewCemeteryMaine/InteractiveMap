637d091aedc23f24b8c7427ed82348c6a22a0ca4
TheTechWiz5305 <silasmorin9@gmail.com>
2024-05-15 06:39:46 -0400

    v0.1.0

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Bayview Cemetery-Bayview Interactive Map-0.1.0";
+const contentToCache = [
+    "Build/Interactive Map v0.1.0.loader.js",
+    "Build/Interactive Map v0.1.0.framework.js",
+    "Build/Interactive Map v0.1.0.data",
+    "Build/Interactive Map v0.1.0.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
